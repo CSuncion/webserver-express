a9370107870e0284f2decae4346746b0ca6aabdf
Cristhian Suncion <csuncion91@outlook.com>
2021-01-15 10:49:34 -0500

    Primer Commit

---
+++ app-old.js
@@ -0,0 +1,13 @@
+const http = require('http');
+http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        let salida = {
+            nombre: 'Cristhian',
+            edad: 29,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        res.end();
+    })
+    .listen(8080);
+console.log(`escuchando puerto 8080`);
